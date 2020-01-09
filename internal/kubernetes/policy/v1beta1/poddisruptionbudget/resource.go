918dbf7a6c31dfd0846b6e1249fbe8309749915c
Nick Schuch <nick@myschuch.com>
2020-01-09 19:02:28 +1000

    Fixes min_available

--- internal/kubernetes/policy/v1beta1/poddisruptionbudget/resource.go
+++ internal/kubernetes/policy/v1beta1/poddisruptionbudget/resource.go
@@ -38,6 +38,10 @@ func Resource() *schema.Resource {
 				Type:     schema.TypeMap,
 				Optional: true,
 			},
+			FieldMinAvailable: {
+				Type:     schema.TypeString,
+				Optional: true,
+			},
 			FieldMatchLabels: {
 				Type:     schema.TypeMap,
 				Optional: true,
