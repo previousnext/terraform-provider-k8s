a7b1081dad07837c52b356914d7d537d1c0d03c0
Nick Schuch <nick@myschuch.com>
2020-09-03 15:33:10 +1000

    Don't delete CRDs

--- internal/kubernetes/apiextensions/v1beta1/crd/delete.go
+++ internal/kubernetes/apiextensions/v1beta1/crd/delete.go
@@ -2,21 +2,10 @@ package crd
 
 import (
 	"github.com/hashicorp/terraform/helper/schema"
-	"github.com/pkg/errors"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-
-	"github.com/previousnext/terraform-provider-k8s/internal/terraform/config"
-	"github.com/previousnext/terraform-provider-k8s/internal/terraform/id"
 )
 
 // Delete the StorageClass.
 func Delete(d *schema.ResourceData, m interface{}) error {
-	conn := m.(*config.Client)
-
-	_, name, err := id.Split(d.Id())
-	if err != nil {
-		return errors.Wrap(err, "failed to delete")
-	}
-
-	return conn.APIExtensions().ApiextensionsV1beta1().CustomResourceDefinitions().Delete(name, &metav1.DeleteOptions{})
+	// We don't delete here. This generally results in very destructive outcomes.
+	return nil
 }
