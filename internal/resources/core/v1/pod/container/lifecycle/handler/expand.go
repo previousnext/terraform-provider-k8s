d00bc710fe14f236d84c56b08c64cdd6c5eaaa4c
Nick Schuch <nick@myschuch.com>
2023-03-07 21:17:48 +1000

    Dependencies

---
+++ internal/resources/core/v1/pod/container/lifecycle/handler/expand.go
@@ -0,0 +1,34 @@
+package handler
+
+import (
+	corev1 "k8s.io/api/core/v1"
+
+	"github.com/previousnext/terraform-provider-k8s/internal/resources/core/v1/pod/container/handler/exec"
+	"github.com/previousnext/terraform-provider-k8s/internal/resources/core/v1/pod/container/handler/http"
+	"github.com/previousnext/terraform-provider-k8s/internal/resources/core/v1/pod/container/handler/tcp"
+)
+
+// Expand will return a structured object.
+func Expand(in []interface{}) *corev1.ProbeHandler {
+	if len(in) == 0 {
+		return nil
+	}
+
+	handler := &corev1.ProbeHandler{}
+
+	raw := in[0].(map[string]interface{})
+
+	if httpval, ok := raw[FieldHTTP]; ok {
+		handler.HTTPGet = http.Expand(httpval.([]interface{}))
+	}
+
+	if tcpval, ok := raw[FieldTCP]; ok {
+		handler.TCPSocket = tcp.Expand(tcpval.([]interface{}))
+	}
+
+	if execval, ok := raw[FieldExec]; ok {
+		handler.Exec = exec.Expand(execval.([]interface{}))
+	}
+
+	return handler
+}
